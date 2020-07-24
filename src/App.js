450942cdb37cebd2f918dda0972a60a3835cfef0
Alaa Shatti <alaa.shatti@gmail.com>
2020-07-24 12:20:56 +0300

    How to use Router and Route in React Project

--- src/App.js
+++ src/App.js
@@ -3,24 +3,42 @@ import Navbar from "./components/Navbar";
 import Users from "./components/Users";
 import  AddUser from "./components/AddUser";
 import './App.css';
+import {BrowserRouter as Router,Route} from "react-router-dom";
 // import Test from "./components/Test";
 
+const Home = () => {
+  return(
+    <h3>Home Page</h3>
+  )
+}
+const About = () => {
+  return(
+    <h3>About Page</h3>
+  )
+}
  class App extends Component{
 
       render() {
 
         return(
+          <Router>
 
-        <div className="container">
-          {/* 
-          Test Clasini cagirma yazim sekli
-          <Test test = "deneme"  /> 
-          */}
-          <Navbar title = "Users App"/>
-          <hr/>
-          <AddUser/>
-         <Users/>
-        </div>
+            <div className="container">
+              <Navbar title = "Users App"/>
+              <hr/>
+              <Route exact path = "/" component = {Users}/>
+              <Route exact path = "/add" component = {AddUser}/>
+              {/* 
+              Test Clasini cagirma yazim sekli
+              <Test test = "deneme"  /> 
+              */}
+
+              {/* <Navbar title = "Users App"/>
+              <hr/>
+              <AddUser/>
+            <Users/> */}
+            </div>
+          </Router>
         
         
       );
