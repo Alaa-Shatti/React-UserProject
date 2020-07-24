69ecf386ad21a98c9fbd2ba3331a4cb32c6d0763
Alaa Shatti <alaa.shatti@gmail.com>
2020-07-25 02:02:01 +0300

    How to use React Route Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import propTypes from "prop-types";
+import {Link} from "react-router-dom";
+
 const Navbar = (props) => {
     return (
         <div>
             <h3>{props.title}</h3>
+            <ul>
+                <li>
+                    <Link to = "/">Home</Link>
+                </li>
+                <li>
+                    <Link to = "/add">Add User</Link>
+                </li>
+            </ul>
         </div>
     )
 }
