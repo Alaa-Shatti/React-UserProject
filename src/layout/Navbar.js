768e79d6d773542bffc542d263c17d6720af0254
Alaa Shatti <alaa.shatti@gmail.com>
2020-07-25 02:46:18 +0300

    Modified Navbar Component

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -2,22 +2,36 @@ import React from 'react'
 import propTypes from "prop-types";
 import {Link} from "react-router-dom";
 
-const Navbar = (props) => {
+const Navbar = ({title}) => {
     return (
-        <div>
-            <h3>{props.title}</h3>
-            <ul>
-                <li>
-                    <Link to = "/">Home</Link>
+        <nav className = "navbar-nav navbar-expand-lg navbar-dark bg-dark mb-3 p-3">
+            <a href = "/" className = " navbar-brand">{title}</a>
+            <ul className = "navbar-nav ml-auto">
+                <li className = "nav-item active mr-3">
+                    <Link to = "/" className = "nav=link text-light">Home</Link>
                 </li>
-                <li>
-                    <Link to = "/add">Add User</Link>
+                <li className = "nav-item active mr-3">
+                    <Link to = "/add" className = "nav=link text-light">Add User</Link>
                 </li>
-                <li>
-                    <Link to = "/github">Project Files</Link>
+                <li className = "nav-item active mr-3">
+                    <Link to = "/github" className = "nav=link text-light">Project Files</Link>
                 </li>
             </ul>
-        </div>
+        </nav>
+        // <div>
+        //     <h3>{title}</h3>
+        //     <ul>
+        //         <li>
+        //             <Link to = "/">Home</Link>
+        //         </li>
+        //         <li>
+        //             <Link to = "/add">Add User</Link>
+        //         </li>
+        //         <li>
+        //             <Link to = "/github">Project Files</Link>
+        //         </li>
+        //     </ul>
+        // </div>
     )
 }
 Navbar.propTypes = {
