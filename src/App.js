f0cef5946504061adad6c26d757d65e282cf522a
Alaa Shatti <alaa.shatti@gmail.com>
2020-07-25 02:20:04 +0300

    How to use Folder in project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
-import Navbar from "./components/Navbar";
+import Navbar from "./layout/Navbar";
 import Users from "./components/Users";
 import  AddUser from "./components/AddUser";
 import './App.css';
 import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
-import NotFoundException from "./components/NotFoundException";
+import NotFoundException from "./pages/NotFoundException";
+import Contribute from './pages/Contribute';
+
 // import Test from "./components/Test";
 
-const Home = () => {
-  return(
-    <h3>Home Page</h3>
-  )
-}
-const About = () => {
-  return(
-    <h3>About Page</h3>
-  )
-}
+// const Home = () => {
+//   return(
+//     <h3>Home Page</h3>
+//   )
+// }
+// const About = () => {
+//   return(
+//     <h3>About Page</h3>
+//   )
+// }
  class App extends Component{
 
       render() {
@@ -30,6 +32,7 @@ const About = () => {
               <Switch>
               <Route exact path = "/" component = {Users}/>
               <Route exact path = "/add" component = {AddUser}/>
+              <Route exact path = "/github" component = {Contribute}/>
               <Route component = {NotFoundException}/>
               </Switch>
               {/* 
